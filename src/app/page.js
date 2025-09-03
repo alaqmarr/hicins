b37a03731dc8fac9d48cde5d8408974be45cd447
Al Aqmar <alaqmarak0810@gmail.com>
2025-09-03 23:05:57 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import HomeSlider2 from '@/app/Components/Home2/HomeSlider2';
-import About2 from '@/app/Components/About/About2';
-import Services2 from '@/app/Components/Home2/Services2';
-import Choose2 from '@/app/Components/Home2/Choose2';
-import Cta from '@/app/Components/Services/Cta';
-import Contact2 from '@/app/Components/Home2/Contact2';
-import Team from '@/app/Components/Home2/Team';
-import Project2 from '@/app/Components/Home2/Project2';
-import Testimonial3 from '@/app/Components/Home2/Testimonial3';
-import Blog2 from '@/app/Components/Home2/Blog2';
+import HomeSlider2 from '../app/Components/Home2/HomeSlider2';
+import About2 from '../app/Components/About/About2';
+import Services2 from '../app/Components/Home2/Services2';
+import Choose2 from '../app/Components/Home2/Choose2';
+import Cta from '../app/Components/Services/Cta';
+import Contact2 from '../app/Components/Home2/Contact2';
+import Team from '../app/Components/Home2/Team';
+import Project2 from '../app/Components/Home2/Project2';
+import Testimonial3 from '../app/Components/Home2/Testimonial3';
+import Blog2 from '../app/Components/Home2/Blog2';
 
 const page = async () => {
     const productsEndpoint = await fetch('https://mystore.alaqmar.dev/api/hic/products', { cache: 'no-store' })
